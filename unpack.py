fafc60da33c33c9c5406028c8629bb6de3c9ab66
peitur <peitur@gmail.com>
2022-11-07 09:49:46 +0100

    kind of working ... ish

--- unpack.py
+++ unpack.py
@@ -3,6 +3,7 @@
 import os, re, sys
 import pathlib, json
 import tarfile
+import logging
 
 import subprocess, shlex
 from pprint import pprint
@@ -11,6 +12,10 @@ from pprint import pprint
 DEFAULT_SOURCE="trailer"
 DEFAULT_TARGET="vendor"
 CARGO_FILE="Cargo.toml"
+
+CRAATE_IMPORT_OVERWRITE=True
+
+
 
 class GenericCommand( object ):
     
@@ -110,22 +115,32 @@ def cargo_meta_file( filepath, filename=CARGO_FILE ):
             
             
 def cargo_meta_pkg( pkgfile ):
-
-    result = list()
-
     try:
         tar = tarfile.open( pkgfile, "r:gz")
         p = re.compile( r"[^/]+/%s" % (CARGO_FILE) )
         for ti in tar.getmembers():
             if p.match( ti.name ):
-                result = [ x.decode("utf-8").lstrip().rstrip() for x in tar.extractfile( ti.name ).readlines() ]
+                return [ x.decode("utf-8").lstrip().rstrip() for x in tar.extractfile( ti.name ).readlines() ]
 
     except KeyError as e:
-        return list()
+        raise IndexError("Invalid crate file")
     finally:
         tar.close()
-    return result
+    raise IndexError("Invalid crate file, no metadata file found")
 
+def cargo_meta_unpack_path( pkgfile ):
+    try:
+        tar = tarfile.open( pkgfile, "r:gz")
+        p = re.compile( r"[^/]+/%s" % (CARGO_FILE) )
+        for ti in tar.getmembers():
+            if p.match( ti.name ):
+                return pathlib.Path( ti.name ).parent
+
+    except KeyError as e:
+        raise IndexError("Invalid crate file")
+    finally:
+        tar.close()
+    raise IndexError("Invalid crate file, no metadata file found")
 
 
 def cargo_meta_parse( data ):
@@ -148,6 +163,36 @@ def cargo_meta_parse( data ):
     return values
 
 
+def cargo_extract_source( cratefile,  tpath="." ):
+    if not cargo_check_source( cratefile ):
+        RuntimeError("Dangerous archive file \"%s\", skipping" % ( cratefile ) )
+    try:
+        tar = tarfile.open( cratefile )
+        tar.extractall( tpath )
+    except:
+        return False
+    finally:
+        tar.close()
+    return True
+
+def cargo_check_source( pkgfile ):
+     
+    rxl = [ re.compile("^/.+"), re.compile("\.\./") ]
+     
+    try:
+        tar = tarfile.open( pkgfile, "r:gz")
+        for ti in tar.getmembers():
+            
+            for rx in rxl:
+                if rx.match( ti.name ):
+                    return False
+
+    except KeyError as e:
+        raise IndexError("Invalid crate file")
+    finally:
+        tar.close()
+    return True
+
 def rmdir_tree( path ):
     pth = pathlib.Path( path )
     for sub in pth.iterdir() :
@@ -157,12 +202,54 @@ def rmdir_tree( path ):
             sub.unlink()
     pth.rmdir()
 
+
 def get_crates( dir ):
     return [ f for f in pathlib.Path( dir ).iterdir() if re.match( ".+\.crate$", f.name ) ]
 
 if __name__ == "__main__":
     
-    pprint( cargo_meta_parse( cargo_meta_pkg( "trailer/chrono-0.4.22.crate") ) )
-    pprint( cargo_meta_parse( cargo_meta_file( "vendor/clap") ) )
+    known_cratews = dict()
+    try:
+        spath = sys.argv[1]
+        tpath = sys.argv[2]
+        
+        crate_list = get_crates( spath )
+        
+        print( "Reading crates from \"%s\"" % ( spath ) )
+        print( "Importing to \"%s\"" % ( tpath ) )
+        print("Found %s crates in \"%s\"" % ( len( crate_list), spath) )
+        for crate in crate_list:
+            filename =  crate.name
+            crt_meta = cargo_meta_parse( cargo_meta_pkg( str( crate ) ) )
+            
+            print(" - \"%s\" = \"%s\" # \"%s\"" % ( crt_meta['package.name'], crt_meta['package.version'], filename ), end=" " )
+
+            if cargo_extract_source( str( crate ), tpath ):
+                crt_unp_name = cargo_meta_unpack_path( str( crate ) )
+                crt_unp_path = pathlib.Path( "%s/%s" % ( tpath, crt_unp_name ) )
+                crt_target = pathlib.Path( "%s/%s" %( tpath, crt_meta['package.name'] ) )
+                crt_rollback_path = pathlib.Path("%s.old" % ( crt_target ) )
+                
+                if not crt_unp_path.exists():
+                    raise OSError("Could not intended crate directory \"%s\"" % ( crt_unp_path ) )
+                
+                if crt_target.exists() and CRAATE_IMPORT_OVERWRITE:
+                    inst_crt_meta = cargo_meta_parse( cargo_meta_file( str( crt_target ) ) )
+                    print( ": Found crate \"%s\" version \"%s\", overwriting with version \"%s\"" % ( inst_crt_meta['package.name'], inst_crt_meta['package.version'], crt_meta['package.version'] ), end=" " )
+                    crt_target.rename( crt_rollback_path )
+                
+                try:
+                    crt_unp_path.rename( crt_target )
+                    if crt_rollback_path.exists( ) and CRAATE_IMPORT_OVERWRITE:
+                        rmdir_tree( str( crt_rollback_path ) )
+                except:
+                    if crt_rollback_path.exists():
+                        crt_rollback_path.rename( crt_target )
+            print()
+            
+    except Exception as e:
+        raise 
+
+
 
     
